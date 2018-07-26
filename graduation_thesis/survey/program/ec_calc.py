a787fd48ea76e6d3e78507f0a47bcb400042564e
yoshikimasubuchi <yoshikimasubuchi@chao-labnoMacBook-puro.local>
2018-07-26 10:15:25 +0900

    update

---
+++ graduation_thesis/survey/program/ec_calc.py
@@ -0,0 +1,12 @@
+cA = 0
+cP = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 - 1
+
+def inv_mod(a, p = cP):
+    lim, him = 1, 0
+
+
+# 技術者向けビットコイン講座 第1回 楕円曲線を自分の手で扱う
+# https://btcnews.jp/elliptc-curve-diy/
+
+# pythonでコードを書きながら楕円曲線暗号を理解する
+# http://swdrsker.hatenablog.com/entry/2018/01/30/073000
---
+++ graduation_thesis/survey/what_is_pairing_ango.md
@@ -0,0 +1,2 @@
+(小ネタ)楕円曲線暗号をなんとなく理解した気になるオススメのリンク4選
+http://blog.livedoor.jp/k_urushima/archives/1742406.html
